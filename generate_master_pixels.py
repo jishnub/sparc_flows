db4c5ffa1519beab42141e9bf1024b8e3a661f39
Jishnu Bhattacharya <jishnuonline@gmail.com>
2018-10-21 09:59:39 +0400

    bugfixes in automatically generating master pixels

--- generate_master_pixels.py
+++ generate_master_pixels.py
@@ -6,10 +6,10 @@ def populate(num_src):
 	datadir = Path(read_params.get_directory())
 	# Choose uniformly random numbers between [-20,20)
 	master_pixels = np.random.random((num_src,1))*40 - 20
-	master_pixels = sorted(master_pixels)
+	master_pixels = np.sort(master_pixels,axis=0)
 	
 	print("master pixels",master_pixels.flatten())
 	print("Writing to {}".format(datadir/"master.pixels"))
 	
-	np.savetxt(datadir/"master.pixels",master_pixels,,fmt="%.1f")
+	np.savetxt(datadir/"master.pixels",master_pixels,fmt="%.1f")
 
